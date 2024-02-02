09c69606ffd2fd3c1acce649a3b18c33d3f8d345
AnimalClass <azisomtochukwu@gmail.com>
2024-02-02 11:50:18 +0100

    second commit with comments

--- animal.js
+++ animal.js
@@ -1,10 +1,11 @@
 class Animal{
-    constructor(){   
+    constructor(){ //an abstract class   
         if(this.constructor === Animal){
             throw new Error ('Animal cannot be called')
         }
     }
 
+    //general methods for abstract class animal
     eat(){
         console.log("Animals can eat")
     }
@@ -24,15 +25,34 @@ class Animal{
 
 //const animal = new Animal()
 
-class invertebrates extends Animal{
+class invertebrates extends Animal{ // subclass invertebrate to perform abstracted animal methods
     noBackbone(){
         console.log('invertebrates have no backbone')
     }
 }
 
 const noBackbone = new invertebrates()
-noBackbone.die()
-noBackbone.eat()
-noBackbone.makeSound()
-noBackbone.move()
-noBackbone.noBackbone()
+
+class vertebrates extends Animal{ // subclass vertebrates to perform abstracted animal methods
+    withBackBone(){
+        console.log('vertebrates have backbone')
+    }
+}
+
+const yesBackBone = new vertebrates()
+
+class warmBlooded extends Animal{ // subclass warmblooded performs abstracted animals
+    regulateTemperature(){
+        console.log('regulates temperature from internal sources')
+    }
+}
+
+const internalRegulator = new warmBlooded()
+
+class coldBlooded extends Animal{ //subclass coldblooded performs abstracted animal methods
+    regulateTemperature(){
+        console.log('regulates temperature from external sources')
+    }
+}
+
+const externalRegulator = new coldBlooded()
